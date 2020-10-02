88f918f9b143447c5bc32a2dc8db8b1231b2b555
Roman Zhuk <rombat999@gmail.com>
2020-10-02 19:59:03 +0300

    feat: carbon and dream team completed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,9 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(str) {
+ if(typeof(str) !== String && str != Number) return false
+  let num = (Math.log(MODERN_ACTIVITY/str.toString(10)))/(0.693/HALF_LIFE_PERIOD)
+return num;
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ if (Array.isArray(members) === false) return false;
+ let arr = [];
+ let str = "";
+ members.sort();
+ console.log(members);
+
+ for (let item of members) {
+  if (typeof item == "string") {
+   str = item.trim();
+
+   arr.push(str[0].toUpperCase());
+  }
+ }
+ return arr.sort().join("");
 };
