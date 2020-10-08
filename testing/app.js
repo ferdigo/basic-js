b82387651403b866e2e3ba8e80d754e3db9bc522
Roman Zhuk <rombat999@gmail.com>
2020-10-08 22:21:27 +0300

    feat: hanoi completed

--- testing/app.js
+++ testing/app.js
@@ -1,22 +1,17 @@
-function getSeason(date) {
-  
-  console.log(date)
-  console.log(date.getFullYear())
-  console.log(date.getMonth())
-  console.log(date.getDate())
 
 
-  let month = date.getMonth()
-let day = date.getDate()
 
-if(month >= 6 && month <9) return 'summer'
-else if (month >= 9 && month <12)  return 'autumn'
-else if (month >= 12 && month <3)  return 'winter'
-else if (month >= 3 && month <6)  return 'spring'
-  
-}
+function calculateHanoi(disksNumber, turnsSpeed) {
 
-console.log(getSeason(new Date(new Date(2019, 11, 22, 23, 45, 11, 500))))
-console.log(getSeason(new Date(2018, 4, 17, 11, 27, 4, 321)));
-console.log(getSeason(new Date(2017, 6, 11, 23, 45, 11, 500)));
-console.log(getSeason(new Date(1994, 8, 26, 3, 0, 11, 500)));
+  let result = {};
+
+  result.turns = 2**disksNumber -1;
+  result.seconds = Math.floor(result.turns*3600/(turnsSpeed));
+
+
+
+  console.log(result)
+};
+
+calculateHanoi(5, 4074);
+calculateHanoi(7, 4090)
